076f4064b88ed9ec0a42289c2b499ad8987a256b
lfernandogh75 <fernandogh75@misena.edu.co>
2026-01-20 19:24:35 -0500

    clase Gerente

--- Mision1/Proyecto1/src/com/empresa/gestion/model/Gerente.java
+++ Mision1/Proyecto1/src/com/empresa/gestion/model/Gerente.java
@@ -1,5 +1,25 @@
 package com.empresa.gestion.model;
 
-public class Gerente {
+public class Gerente extends Empleado {
+    private String departamento;
+
+    public Gerente(String nombre, int edad, double salario, String departamento) {
+        super(nombre, edad, salario);
+        this.departamento = departamento;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public void presentarse() {
+        System.out.println("Soy el gerente " + getNombre()  + " y estoy a cargo del departamento de " + departamento);
+    }
+
     
 }
